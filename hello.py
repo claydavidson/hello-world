a680d266525868dccf82f99855b05206b74866cd
Clay Davidson <claydavidson@users.noreply.github.com>
2016-07-31 00:30:38 -0500

    Create hello.py
    Created a python file to print out hello world a couple of different ways.

---
+++ hello.py
@@ -0,0 +1,17 @@
+# hello.py
+#
+# Hello world program in python to use to figure out how Github works
+#
+# Also learning python now so use it for anything I want to test out in python too.
+#
+#
+
+var1 = "World"
+nl = "\n"
+
+print("hello " + var1 + "\n")
+print("hello",var1,nl)
+
+return 0 # -- not sure if you return from a main function or not but figured it can't hurt
+
+
