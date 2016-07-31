b2d89590867103f8ca399653fa6481e987eff539
Clay Davidson <claydavidson@users.noreply.github.com>
2016-07-31 00:48:27 -0500

    Update hello.py
    Added a function call

--- hello.py
+++ hello.py
@@ -3,15 +3,24 @@
 # Hello world program in python to use to figure out how Github works
 #
 # Also learning python now so use it for anything I want to test out in python too.
-#
+# Remove the parenthesis from the print statement if using version 2.x....changed the way it worked in 3.x
 #
 
 var1 = "World"
+var2 = "Hello "
 nl = "\n"
 
+print("hello", " ", var1)
 print("hello " + var1 + "\n")
 print("hello",var1,nl)
+print(var2,var1,nl)
+print("Hello World\n")
 
-return 0 # -- not sure if you return from a main function or not but figured it can't hurt
-
+mystring = "Corky, you handsome devil you."
+hw(mystring)
+ # return 0 -- got an error that can not have a return outside of a function
+ 
+def hw(strvar): # need to put a colon after function definitions and soem other stuff. Not exactly sure where all it goes yet.
+  return print("Hello ", strvar) #must indent instead of using squiggly braces
+  
 
